4a1e833219d56929cea565ce6b243b42ec2af178
Shashank <104366910+NDFA-with-epsilon@users.noreply.github.com>
2022-10-04 20:21:46 +0530

    merge sort sorted

--- Kruskal/msort_tmp.h
+++ Kruskal/msort_tmp.h
@@ -9,8 +9,8 @@
 #define INVALID_PARAM -1
 #define SUCCESS 1
 
-int merge_stub(adj_list* list, int vertex);
-void sort_list(adj_list* list, int vertex /* 1,2,3... */, int l, int r); 
-void merge(adj_list* list, int vertex, int l, int mid, int r);
+int merge_stub(adj_list* g_list, int vertex);
+void sort_list(adj_list* g_list, int vertex /* 1,2,3... */, int l, int r); 
+void merge(adj_list* g_list, int vertex, int l, int mid, int r);
 
 #endif
